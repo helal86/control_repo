06a1b33ff9fbdf695c8f6baa73b17011098b9fe7
Helal Khan <helal86@users.noreply.github.com>
2020-06-23 14:47:49 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,8 @@
 node default {
   file {'/root/README':
-  ensure => file,
-  content => 'This is a readme file',
+  ensure   => file,
+  content  => 'This is a readme file',
+  owner    => 'root',
   }
 
 }
