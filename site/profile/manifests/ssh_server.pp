97074d6a40d6775b01e5ee46e142380db65b2242
Helal Khan <helal86@users.noreply.github.com>
2020-06-24 15:59:06 +0100

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC4M6GXJlCXQLIiDODmTLBY2t8KSHDllT5al3T9UIj1uvcyMolSWzVx1HkIXQMQXTUEaJLNITasbpq/Dlh2SBIVp+6huh/3ssOmVjbxI8kXEB1WjqDdpImmxnwmtpdqIoqzxRd4LnJjUhKHI8qEkKoSEYXyXj0WBuXUi6DuP9szCruThj8DLxRsR/xSvzg1iS4MchDiRReGO0F+sSZMkAIoODIBqW2ZHfwF6pk+GSL3fP7w9UXNQzSqNysS/e6/o8bv0De7ZBnsuboAfTxQ70YXYsl8aYueeUolgBZ+d150Q1rt8FQERy7vIMaXa3L9vJ4MmugIaSrB+0XYgIqUuZAh root@master.puppet.vm',
 	}  
 }
